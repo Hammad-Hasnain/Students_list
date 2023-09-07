eeda3a89523f383e37fa220786e7bc6033d18616
Hammad-Hasnain <142816578+Hammad-Hasnain@users.noreply.github.com>
2023-09-07 06:01:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,64 +1,17 @@
 var tBody = document.getElementById('tbody')
-console.log(tBody)
-
-var student1 = {
-    firstName: "john",
-    lastName: "Doe",
-    age: 200,
-    id: Math.random(),
-};
-
-var student2 = {
-    firstName: "king",
-    lastName: "Hashmi",
-    age: 100,
-    id: Math.random(),
-};
-var student3 = {
-    firstName: "Prince",
-    lastName: "Ali",
-    age: 499,
-    id: Math.random(),
-};
-var student4 = {
-    firstName: "Xtylish",
-    lastName: "Hasan",
-    age: 999,
-    id: Math.random(),
-};
-var student5 = {
-    firstName: "lol",
-    lastName: "lol",
-    age: 0,
-    id: Math.random(),
-};
-
-
-// var studentList = [student1, student2, student3, student4, student5]
-// console.log(studentList)
-
-var studentList = []
-console.log(studentList)
-
-var inp1 = document.getElementById('inp1')
-var inp2 = document.getElementById('inp2')
-var inp3 = document.getElementById('inp3')
+var studentList = []   //array of objects
+var inp1 = document.getElementById('inp1')   // firstname
+var inp2 = document.getElementById('inp2')   // lastname
+var inp3 = document.getElementById('inp3')   // age
 
 function addValue() {
-
     tBody.innerHTML = ''
-
     var a = inp1.value
     var b = inp2.value
     var c = inp3.value
-    console.log(a)
-    console.log(b)
-    console.log(c)
-
     inp1.value = ''
     inp2.value = ''
     inp3.value = ''
-
     var obj = {
         firstName: a,
         lastName: b,
@@ -66,11 +19,8 @@ function addValue() {
         id: Math.round(Math.random() * 100 + 10 - 3),
     }
     studentList.push(obj)
-    console.log(studentList)
-
     var totStds = document.getElementById('totStds')
     totStds.innerHTML = `${studentList.length}`
-    // console.log(totStds.innerHTML)
 
     for (var i = 0; i < studentList.length; i++) {
         tBody.innerHTML += ` <tr>
@@ -80,10 +30,7 @@ function addValue() {
                                  <td>${studentList[i].age}</td>
                                  <td>${studentList[i].id}</td>
                              </tr>`
-
     }
-
-
 }
 
 
